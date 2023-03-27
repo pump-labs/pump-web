02ced171b53d1976fad118bbbfcdc5f14e5ff0fc
youngmin wi <youngminieo1005@gmail.com>
2023-03-27 23:40:50 +0900

    :ambulance: Step 2 이미지 첨부 Overflow 해결

--- components/feature/ShowImageContent/index.tsx
+++ components/feature/ShowImageContent/index.tsx
@@ -1,6 +1,6 @@
-import { DeleteBar, ShowImageContainer } from './styled';
 import Image from 'next/image';
 import { CancelIcon } from 'public/static/icons';
+import { DeleteBar, ShowImageContainer } from './styled';
 
 type Props = {
 	src: string;
@@ -11,7 +11,7 @@ type Props = {
 const ShowImageContent = ({ width, height, src, alt, ...props }: Props) => {
 	return (
 		<ShowImageContainer width={width} height={height}>
-			<Image src={src} alt={alt} width={width} height={height} />
+			<Image src={src} alt={alt} priority={true} fill={true} />
 			<DeleteBar width={width}>
 				<CancelIcon onClick={props.onClick} />
 			</DeleteBar>
